3cf016e10a0bc2b74ab507dbe93e2b03e03269f0
Wellington Garcia <wellington.garcia@bancointer.com.br>
2021-10-10 17:53:16 -0300

    integration cart project to container (host)

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
       name: 'container',
       remotes: {
         products: 'products@http://localhost:8081/remoteEntry.js',
+        cart: 'cart@http://localhost:8082/remoteEntry.js',
       },
     }),
     new HtmlWebpackPlugin({
