9fa652387045560f8ffb5b31ddc672c99b92a025
Wellington Garcia <garciawell@gmail.com>
2021-10-11 20:04:37 -0300

    webpack dev and public folder

--- packages/marketing/config/webpack.common.js
+++ packages/marketing/config/webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-react','@babel/preset-env'],
-                        plugins: ['@babel/plugins-transform-runtime']
+                        plugins: ['@babel/plugin-transform-runtime']
                     }
                 }
             }
