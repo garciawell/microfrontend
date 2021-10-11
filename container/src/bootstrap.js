54ac228a3ad226c0245b23a4550b7144fccfca2e
Wellington Garcia <wellington.garcia@bancointer.com.br>
2021-10-11 19:49:27 -0300

    feat: star new project

--- container/src/bootstrap.js
+++
@@ -1,7 +0,0 @@
-import { mount as mountProducts } from 'products/ProductsIndex';
-import { mount as mountCart } from 'cart/CartShow'; 
-
-console.log('Container!');
-
-mountProducts(document.querySelector("#my-products"))
-mountCart(document.querySelector("#my-cart"))
