d37bec981eecbd34cc1d55750c7680e3857c389f
Wellington Garcia <wellington.garcia@bancointer.com.br>
2021-10-11 20:19:08 -0300

    feat: marketing landing and pricing page

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 
 const mount = (el) => {
-    ReactDOM.render(
-        <h1>HELLO</h1>, 
-        el
-    )
+    ReactDOM.render(<App />, el)
 }
 
 if(process.env.NODE_ENV == 'development') {
