2e5dcfa987fa9d41bb92ef3fb2cec4a275e2220d
Wellington Garcia <wellington.garcia@bancointer.com.br>
2021-10-11 20:57:59 -0300

    sync shared dependencies

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJSON = require('../package.json')
 
 const devConfig = {
     mode: 'development',
@@ -20,7 +21,8 @@ const devConfig = {
             filename: 'remoteEntry.js',
             remotes: {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js'
-            }
+            },
+            shared: packageJSON.dependencies
         })
 
     ]
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJSON = require('../package.json')
 
 const devConfig = {
     mode: 'development',
@@ -20,7 +21,8 @@ const devConfig = {
             filename: 'remoteEntry.js',
             exposes: {
                 './MarketingApp': './src/bootstrap'
-            }
+            },
+            shared: packageJSON.dependencies
         })
     ]
 }
