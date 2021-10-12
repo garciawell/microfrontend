62489a8ca1de6ed1e249c8d66daffe81568c412e
Wellington Garcia <garciawell@gmail.com>
2021-10-12 16:27:30 -0300

    system navigation BrowserHistory and MemoryHistory with callback

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -8,9 +8,7 @@ const devConfig = {
     mode: 'development',
     devServer: {
         port: 8080,
-        historyApiFallback: {
-            index: 'index.html'
-        }
+        historyApiFallback: true
     },
     plugins: [
         new ModuleFederationPlugin({
--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,21 @@
 import React, {useEffect, useRef} from 'react';
 import { mount } from 'marketing/MarketingApp'
+import { useHistory } from 'react-router-dom';
 
 function MarketingApp() {
-    const marketingRef = useRef(null)
+    const marketingRef = useRef(null);
+    const history = useHistory();
 
     useEffect(() => {
-        mount(marketingRef.current)
+        const { onParentNavigate } = mount(marketingRef.current, {
+            onNavigate: ({ pathname: nextPathname }) => {
+                if(history.location.pathname !== nextPathname) {
+                    history.push(nextPathname) 
+                }
+            }
+        })
+
+        history.listen(onParentNavigate)
     }, [])
 
 
