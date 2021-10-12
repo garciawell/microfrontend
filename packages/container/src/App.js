630476f052b463b8a3a173741bc83864863db380
Wellington Garcia <wellington.garcia@bancointer.com.br>
2021-10-12 15:33:25 -0300

    fix: css conflict projects

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'rt'
+})
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   )
 }
 
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter} from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
-export default () => {
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma'
+})
+
+export default () => { 
     return <div>
-        <StylesProvider>
+        <StylesProvider generateClassName={generateClassName}>
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/pricing" component={Pricing} />
