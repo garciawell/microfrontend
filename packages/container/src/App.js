5ccef4976e1b4e6a7888b3b973d0dc6a76c52340
Wellington Garcia <garciawell@gmail.com>
2021-10-12 14:41:27 -0300

    ciÇ test new cache system

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
-import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import MarketingApp from './components/MarketingApp';
 
 function App() {
   return (
