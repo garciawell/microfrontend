ce50a2beebad50c7f51e10830250ab4923e59397
Wellington Garcia <wellington.garcia@bancointer.com.br>
2021-10-11 22:54:14 -0300

    create ci/cd github actions

---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,23 @@
+const { merge }  = require('webpack-merge')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJSON = require('../package.json');
+
+const prodConfig = {
+    mode: "production",
+    output: {
+        filename: '[name].[contenthash].js'
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingApp': './src/bootstrap'
+            },
+            shared: packageJSON.dependencies
+        })
+    ]
+};
+
+module.exports = merge(commonConfig, prodConfig)
