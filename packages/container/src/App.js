0eb35dffb724bd22956e4fe65b712453cd4e18d6
Wellington Garcia <garciawell@gmail.com>
2021-10-12 14:25:24 -0300

    feat: add header root project

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
 function App() {
   return (
-    <div>
-      HELLO
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   )
 }
 
