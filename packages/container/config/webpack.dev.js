89b6e79c85236b3556fca4325e13cb06b312664a
Wellington Garcia <garciawell@gmail.com>
2021-10-11 22:37:33 -0300

    config webpack  production

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJSON = require('../package.json')
@@ -13,9 +13,6 @@ const devConfig = {
         }
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        }),
         new ModuleFederationPlugin({
             name: 'container',
             filename: 'remoteEntry.js',
@@ -24,7 +21,6 @@ const devConfig = {
             },
             shared: packageJSON.dependencies
         })
-
     ]
 }
 
